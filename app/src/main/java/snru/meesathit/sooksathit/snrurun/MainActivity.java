f78e42bd2e79506bffb657e4a805e2e4023d5450
meen <smeesathit@gmail.com>
2016-05-15 23:33:59 -0700

    SignUp Opened

--- app/src/main/java/snru/meesathit/sooksathit/snrurun/MainActivity.java
+++ app/src/main/java/snru/meesathit/sooksathit/snrurun/MainActivity.java
@@ -1,7 +1,9 @@
 package snru.meesathit.sooksathit.snrurun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    } // End onCreate method
+
+    public void clickSignUpMain(View view){
+        startActivity(new Intent(MainActivity.this, SignUp.class));
+    } // End clickSignUpMain method
+} // End main class
