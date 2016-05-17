afb45e92a14d31c6ef6035284d3960de8e839526
meen <smeesathit@gmail.com>
2016-05-17 00:00:53 -0700

    Add Data

--- app/src/main/java/snru/meesathit/sooksathit/snrurun/SignUp.java
+++ app/src/main/java/snru/meesathit/sooksathit/snrurun/SignUp.java
@@ -1,114 +1,126 @@
 package snru.meesathit.sooksathit.snrurun;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
-import com.squareup.okhttp.Call;
-import com.squareup.okhttp.Callback;
-import com.squareup.okhttp.FormEncodingBuilder;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
-import com.squareup.okhttp.Response;
 
-import java.io.IOException;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.view.View;
+        import android.widget.EditText;
+        import android.widget.RadioButton;
+        import android.widget.RadioGroup;
+
+        import com.squareup.okhttp.Call;
+        import com.squareup.okhttp.Callback;
+        import com.squareup.okhttp.FormEncodingBuilder;
+        import com.squareup.okhttp.OkHttpClient;
+        import com.squareup.okhttp.Request;
+        import com.squareup.okhttp.RequestBody;
+        import com.squareup.okhttp.Response;
+
+        import java.io.IOException;
 
 public class SignUp extends AppCompatActivity {
-    // Variable declaration
-    private EditText nameEditText;
-    private EditText userEditText;
-    private EditText passwordEditText;
 
+    //Explicit
+    private EditText nameEditText, userEditText, passwordEditText;
     private RadioGroup radioGroup;
+    private RadioButton choice1RadioButton, choice2RadioButton,
+            choice3RadioButton, choice4RadioButton, choice5RadioButton;
+    private String nameString, userString, passwordString, avataString = "0";
 
-    private RadioButton choice1RadioButton, choice2RadioButton, choice3RadioButton, choice4RadioButton, choice5RadioButton;
 
-    private String nameString, userString, passwordString, avatarString = "0";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        //Bind Widget
         bindWidget();
 
-        // Radio Button
+        //Radio Button Controller
         radioButtonController();
 
 
-    } // End method onCreate
+
+    }   // Main Method
 
     private void radioButtonController() {
+
         radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
                 switch (i) {
                     case R.id.radioButton:
-                        avatarString = "0";
+                        avataString = "0";
                         break;
                     case R.id.radioButton2:
-                        avatarString = "1";
+                        avataString = "1";
                         break;
                     case R.id.radioButton3:
-                        avatarString = "2";
+                        avataString = "2";
                         break;
                     case R.id.radioButton4:
-                        avatarString = "3";
+                        avataString = "3";
                         break;
                     case R.id.radioButton5:
-                        avatarString = "4";
+                        avataString = "4";
                         break;
                 }
+
             }
         });
     }
 
     private void bindWidget() {
+
         nameEditText = (EditText) findViewById(R.id.nameEditText);
         userEditText = (EditText) findViewById(R.id.userEditText);
         passwordEditText = (EditText) findViewById(R.id.passwordEditText);
-
         radioGroup = (RadioGroup) findViewById(R.id.ragAvatar);
-
         choice1RadioButton = (RadioButton) findViewById(R.id.radioButton);
         choice2RadioButton = (RadioButton) findViewById(R.id.radioButton2);
         choice3RadioButton = (RadioButton) findViewById(R.id.radioButton3);
         choice4RadioButton = (RadioButton) findViewById(R.id.radioButton4);
-        choice5RadioButton = (RadioButton) findViewById(R.id.radioButton5);
+
     }
 
     public void clickSignUpSign(View view) {
+
+        //Get Value From EditText
         nameString = nameEditText.getText().toString().trim();
         userString = userEditText.getText().toString().trim();
         passwordString = passwordEditText.getText().toString().trim();
 
-        // Check empty input
-        if (nameString.equals("") || userEditText.equals("") || passwordEditText.equals("")) {
+        //Check Space
+        if (nameString.equals("") || userString.equals("") || passwordString.equals("") ) {
+
             MyAlert myAlert = new MyAlert();
-            myAlert.myDialog(this, "Empty Box!", "Fill all boxes!");
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณากรอกทุกช่องคะ");
+
         } else {
+
             updateUserTABLE();
-            
+
         }
 
 
 
-    } // End method clickSignUpSign
+    }
 
     private void updateUserTABLE() {
+
         OkHttpClient okHttpClient = new OkHttpClient();
         RequestBody requestBody = new FormEncodingBuilder()
                 .add("isAdd", "true")
                 .add("Name", nameString)
                 .add("User", userString)
                 .add("Password", passwordString)
-                .add("Avatar", avatarString)
+                .add("Avata", avataString)
                 .build();
         Request.Builder builder = new Request.Builder();
-        Request request = builder.url("http://swiftcodingthai.com/snru/add_user_meen.php").post(requestBody).build();
+        Request request = builder.url("http://swiftcodingthai.com/snru/add_user_master.php").post(requestBody).build();
+        //Request request = builder.url("http://swiftcodingthai.com/snru/add_user_meen.php").post(requestBody).build();
 
         Call call = okHttpClient.newCall(request);
         call.enqueue(new Callback() {
@@ -123,5 +135,8 @@ public class SignUp extends AppCompatActivity {
             }
         });
 
-    }
-} // End class SignUp
+
+
+    }   // updateUserTABLE
+
+}   // Main Class
