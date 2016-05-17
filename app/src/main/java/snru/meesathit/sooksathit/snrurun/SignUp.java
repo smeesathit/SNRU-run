50752302224bc83c1a46bdcfb544e66479a04150
meen <smeesathit@gmail.com>
2016-05-16 23:26:44 -0700

    Updata

--- app/src/main/java/snru/meesathit/sooksathit/snrurun/SignUp.java
+++ app/src/main/java/snru/meesathit/sooksathit/snrurun/SignUp.java
@@ -7,6 +7,16 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignUp extends AppCompatActivity {
     // Variable declaration
     private EditText nameEditText;
@@ -17,7 +27,7 @@ public class SignUp extends AppCompatActivity {
 
     private RadioButton choice1RadioButton, choice2RadioButton, choice3RadioButton, choice4RadioButton, choice5RadioButton;
 
-    private String nameString, userString, passwordString, avatarString;
+    private String nameString, userString, passwordString, avatarString = "0";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +35,36 @@ public class SignUp extends AppCompatActivity {
 
         bindWidget();
 
+        // Radio Button
+        radioButtonController();
+
 
     } // End method onCreate
+
+    private void radioButtonController() {
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                switch (i) {
+                    case R.id.radioButton:
+                        avatarString = "0";
+                        break;
+                    case R.id.radioButton2:
+                        avatarString = "1";
+                        break;
+                    case R.id.radioButton3:
+                        avatarString = "2";
+                        break;
+                    case R.id.radioButton4:
+                        avatarString = "3";
+                        break;
+                    case R.id.radioButton5:
+                        avatarString = "4";
+                        break;
+                }
+            }
+        });
+    }
 
     private void bindWidget() {
         nameEditText = (EditText) findViewById(R.id.nameEditText);
@@ -52,9 +90,38 @@ public class SignUp extends AppCompatActivity {
             MyAlert myAlert = new MyAlert();
             myAlert.myDialog(this, "Empty Box!", "Fill all boxes!");
         } else {
+            updateUserTABLE();
+            
         }
 
 
 
     } // End method clickSignUpSign
+
+    private void updateUserTABLE() {
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd", "true")
+                .add("Name", nameString)
+                .add("User", userString)
+                .add("Password", passwordString)
+                .add("Avatar", avatarString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url("http://swiftcodingthai.com/snru/add_user_meen.php").post(requestBody).build();
+
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+                finish();
+            }
+        });
+
+    }
 } // End class SignUp
