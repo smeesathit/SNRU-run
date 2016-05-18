0aca0894b03bf75bc02dd6ca2792af576a4334f3
meen <smeesathit@gmail.com>
2016-05-17 17:14:12 -0700

    Insert Data from JASON

--- app/src/main/java/snru/meesathit/sooksathit/snrurun/MainActivity.java
+++ app/src/main/java/snru/meesathit/sooksathit/snrurun/MainActivity.java
@@ -1,9 +1,20 @@
 package snru.meesathit.sooksathit.snrurun;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.AsyncTask;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +27,73 @@ public class MainActivity extends AppCompatActivity {
 
         myManage = new MyManage(MainActivity.this);
 
+        // Test add user
+        //myManage.addUser("Meen", "Disney", "12345", "1");
+
+        // Delete all from table
+        deleteAllSQLite();
+
+        // Synchronize
+        MySynchronize mySynchronize = new MySynchronize();
+        mySynchronize.execute();
+
         
     } // End onCreate method
+
+    // Create Inner Class
+    public class MySynchronize extends AsyncTask<Void, Void, String> {
+
+
+        @Override
+        protected String doInBackground(Void... voids) {
+
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url("http://swiftcodingthai.com/snru/get_user.php").build();
+                Response response = okHttpClient.newCall(request).execute();
+
+                return response.body().string();
+            } catch (Exception e) {
+                return null;
+            }
+            //return null;
+        } //End method doInBackground
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("Snru", "JSON == " + s);
+
+            try {
+                JSONArray jsonArray = new JSONArray(s);
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String  strName = jsonObject.getString(MyManage.column_name);
+                    String  strUser = jsonObject.getString(MyManage.column_user);
+                    String  strPassword = jsonObject.getString(MyManage.column_password);
+                    String  strAvata = jsonObject.getString(MyManage.column_avata);
+
+                    myManage.addUser(strName, strUser, strPassword, strAvata);
+
+                } // end for
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    } // End Inner Class MySynchronize
+
+
+    private void deleteAllSQLite() {
+        // MODE_PRIVATE = delete only
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name, MODE_PRIVATE, null);
+
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+
+    }
+
 
     public void clickSignUpMain(View view){
         startActivity(new Intent(MainActivity.this, SignUp.class));
--- app/src/main/java/snru/meesathit/sooksathit/snrurun/MyManage.java
+++ app/src/main/java/snru/meesathit/sooksathit/snrurun/MyManage.java
@@ -1,5 +1,6 @@
 package snru.meesathit.sooksathit.snrurun;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -11,9 +12,29 @@ public class MyManage {
     private MyOpenHelper myOpenHelper;
     private SQLiteDatabase sqLiteDatabase;
 
+    public static  final String user_table = "userTABLE";
+    public static  final String column_id = "_id";
+    public static  final String column_name = "Name";
+    public static  final String column_user = "User";
+    public static  final String column_password = "Password";
+    public static  final String column_avata = "Avata";
+
+
     public MyManage(Context context) {
         myOpenHelper = new MyOpenHelper(context);
         sqLiteDatabase = myOpenHelper.getWritableDatabase();
+    }
+
+    public long addUser(String strName, String strUser, String strPassword, String strAvata) {
+
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(column_name, strName);
+        contentValues.put(column_user, strUser);
+        contentValues.put(column_password, strPassword);
+        contentValues.put(column_avata, strAvata);
+
+        return sqLiteDatabase.insert(user_table, null, contentValues);
     }
 
 }
